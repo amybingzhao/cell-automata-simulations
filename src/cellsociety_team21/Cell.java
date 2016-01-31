4e9626048795f24021ef4916f07613bb779fcb82
azhao96 <azhao96@gmail.com>
2016-01-30 23:29:39 -0500

    Added fields for Cell and getters/setters

--- src/cellsociety_team21/Cell.java
+++ src/cellsociety_team21/Cell.java
@@ -1,9 +1,48 @@
 package cellsociety_team21;
 
 public class Cell {
+	private String myCurState;
+	private String myNextState;
+	private int myRow;
+	private int myCol;
 	
-	public Cell(int initialState) {
-		
+	public Cell(String initialState, int r, int c) {
+		myCurState = initialState;
+		myRow = r;
+		myCol = c;
+		myNextState = null;
+	}
+	
+	public int getRow() {
+		return myRow;
+	}
+	
+	public void setRow(int r) {
+		myRow = r;
+	}
+	
+	public int getCol() {
+		return myCol;
+	}
+	
+	public void setCol(int c) {
+		myCol = c;
+	}
+	
+	public String getCurState() {
+		return myCurState;
+	}
+	
+	public void setCurState(String s) {
+		myCurState = s;
+	}
+	
+	public String getNextState() {
+		return myNextState;
+	}
+	
+	public void setNextState(String s) {
+		myNextState = s;
 	}
 
 }
