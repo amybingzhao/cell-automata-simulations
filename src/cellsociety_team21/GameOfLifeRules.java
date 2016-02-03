13c0ed7da4be4f471552d3601189eaa5c33504a9
azhao96 <azhao96@gmail.com>
2016-02-02 20:31:11 -0500

    commented GameOfLifeRules

--- src/cellsociety_team21/GameOfLifeRules.java
+++ src/cellsociety_team21/GameOfLifeRules.java
@@ -7,11 +7,15 @@ public class GameOfLifeRules extends Rules {
 	private static final int MY_CELL_ROW = 1;
 	private static final int MY_CELL_COL = 1;
 	
+	/**
+	 * Apply the rules of the Game of Life simulation to each Cell based on its state.
+	 */
 	@Override
 	public void applyRulesToCell(Cell cell, Grid grid) {
 		String curState = cell.getCurState();
 		Cell[][] neighborhood = grid.getNeighborhood(cell.getCurRow(), cell.getCurCol(), NUM_NEIGHBORS);
 		int numLiveNeighbors = countNumLiveNeighbors(neighborhood);
+		
 		if (curState.equals(DEAD)) {
 			handleDeadCells(cell, numLiveNeighbors);
 		} else {
@@ -19,6 +23,11 @@ public class GameOfLifeRules extends Rules {
 		}
 	}
 
+	/**
+	 * Counts the number of live neighbors a Cell has.
+	 * @param neighborhood: 3x3 array of Cells with the Cell of interest in the center and its neighbors surrounding it.
+	 * @return number of live neighbors.
+	 */
 	private int countNumLiveNeighbors(Cell[][] neighborhood) {
 		int numNeighbors = 0;
 		
@@ -37,6 +46,11 @@ public class GameOfLifeRules extends Rules {
 		return numNeighbors;
 	}
 
+	/**
+	 * If a live Cell has < 2 neighbors, it dies of underpopulation; if it has > 3 neighbors, it dies of overpopulation.
+	 * @param cell: alive Cell of interest.
+	 * @param numLiveNeighbors: number of live neighbors the cell has.
+	 */
 	private void handleAliveCells(Cell cell, int numLiveNeighbors) {
 		if (numLiveNeighbors < 2 || numLiveNeighbors > 3) {
 			cell.setNextState(DEAD);
@@ -44,6 +58,11 @@ public class GameOfLifeRules extends Rules {
 		}
 	}
 
+	/**
+	 * If a dead Cell has exactly 3 live neighbors, it becomes alive as though through reproduction.
+	 * @param cell: dead Cell of interest.
+	 * @param numLiveNeighbors: number of live neighbors the cell has.
+	 */
 	private void handleDeadCells(Cell cell, int numLiveNeighbors) {
 		if (numLiveNeighbors == 3) {
 			cell.setNextState(ALIVE);
