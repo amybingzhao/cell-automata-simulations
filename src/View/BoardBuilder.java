7517fc4789af173ee92ed0c12f171389e409ccd6
blakejkaplan <blakejkaplan@gmail.com>
2016-02-16 22:06:53 -0500

    Masterpiece Commit

--- src/View/BoardBuilder.java
+++ src/View/BoardBuilder.java
@@ -20,37 +20,44 @@ public abstract class BoardBuilder {
 	protected int maxCellsDisplayed;
 	protected int myGridWidth;
 	protected int myGridHeight;
-	
+
 	public static final String DEFAULT_VIEW_RESOURCE = "View/View";
 	protected ResourceBundle myViewResources;
-	
+
 	protected CSView myView;
 	protected Simulation mySimulation;
-	
-	public BoardBuilder(CSView view, Simulation sim){
+
+	public BoardBuilder(CSView view, Simulation sim) {
 		myView = view;
 		mySimulation = sim;
 		myViewResources = ResourceBundle.getBundle(DEFAULT_VIEW_RESOURCE);
 		myGridWidth = 0;
 		myGridHeight = 0;
 		loadResources(myViewResources);
-		
+
 	}
-	
-	private void loadResources(ResourceBundle r){
+
+	/**
+	 * Sets instance variables according to contents of provided resource bundle
+	 * 
+	 * @param r
+	 *            A given resource bundle
+	 */
+	private void loadResources(ResourceBundle r) {
 		boardPixelSize = Integer.parseInt(r.getString(BOARD_PIXEL_SIZE));
 		borderPixelSize = Integer.parseInt(r.getString(DEFAULT_BORDER_PIXEL_SIZE));
 		maxCellsDisplayed = Integer.parseInt(r.getString(DEFAULT_MAX_CELLS_DISPLAYED));
 	}
-	
+
 	/**
-	 * builds board by adding rectangles, with the size and quantity based on xml input
-	 * If board shape changes, rebuilds board, otherwise just redisplays
+	 * builds board by adding rectangles, with the size and quantity based on
+	 * xml input If board shape changes, rebuilds board, otherwise just
+	 * redisplays
 	 */
-	protected void displayBoard(Group myBoardGroup){
+	protected void displayBoard(Group myBoardGroup) {
 		int currentGridWidth = mySimulation.getGrid().getNumCols();
 		int currentGridHeight = mySimulation.getGrid().getNumRows();
-		if(currentGridWidth != myGridWidth || currentGridHeight != myGridHeight){
+		if (currentGridWidth != myGridWidth || currentGridHeight != myGridHeight) {
 			myGridWidth = currentGridWidth;
 			myGridHeight = currentGridHeight;
 			buildBoard(myBoardGroup);
@@ -58,17 +65,36 @@ public abstract class BoardBuilder {
 		displayGridToBoard();
 	}
 
+	/**
+	 * Returns the border pixel size
+	 * 
+	 * @return The border pixel size
+	 */
 	protected int getBorderPixelSize() {
 		return borderPixelSize;
 	}
 
+	/**
+	 * Changes the border pixel size
+	 * 
+	 * @param newBorderPixelSize
+	 *            The desired new pixel size
+	 */
 	protected void setBorderPixelSize(int newBorderPixelSize) {
-		this.borderPixelSize = borderPixelSize;
+		this.borderPixelSize = newBorderPixelSize;
 	}
 
+	/**
+	 * Abstract method to set up the board
+	 * 
+	 * @param myBoardGroup
+	 *            A group contained nodes to be included
+	 */
 	protected abstract void buildBoard(Group myBoardGroup);
-	
+
+	/**
+	 * Abstract method to define behavior for displaying data to the board
+	 */
 	protected abstract void displayGridToBoard();
-	
 
 }
