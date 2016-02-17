1dbf870cb426b1799c6eb765db316f543007c3ef
azhao96 <azhao96@gmail.com>
2016-02-16 21:17:30 -0500

    grouped related methods

--- src/Rules/Rules.java
+++ src/Rules/Rules.java
@@ -29,15 +29,11 @@ public abstract class Rules {
 	private String myDefaultState;
 	
 	/**
-	 * Helper method that applies the specified rules to each method in the grid
+	 * Rules for each simulation to be implemented by simulation-specific subclasses.
+	 * @param cell: Cell to apply rules to.
+	 * @param grid: Simulation grid.
 	 */
-	public void applyRulesToGrid(Grid grid){
-		for(int r = 0; r < grid.getNumRows(); r++){
-			for(int c = 0; c < grid.getNumCols(); c++){
-				applyRulesToCell(grid.getCell(r,c), grid);
-			}
-		}
-	}
+	public abstract void applyRulesToCell(Cell cell, Grid grid);
 	
 	/**
 	 * Initialize the Grid with the Cells corresponding to this simulation.
@@ -55,26 +51,6 @@ public abstract class Rules {
 	}
 	
 	/**
-	 * Populates the state and colors information into two maps
-	 */
-	public void populateStatesInfo(){
-		myStatesCount = new HashMap<String, Integer>();
-		myStatesColors = new HashMap<String, Color>();
-		myRulesResources = ResourceBundle.getBundle(DEFAULT_RULES_RESOURCE);
-		String ruleName = toString().replace(" ", "");
-		String[] states = myRulesResources.getString(ruleName + "States").split(",");
-		String[] colors = myRulesResources.getString(ruleName + "Colors").split(",");
-		for(int i = 0; i < states.length; i++){
-			myStatesCount.put(states[i], 0);
-			Color color = Color.web(colors[i]);
-			myStatesColors.put(states[i], color);
-		}
-		myDefaultState = myRulesResources.getString(ruleName + "DefaultState");
-		myCellRow = Integer.parseInt(myRulesResources.getString("MyCellRow"));
-		myCellCol = Integer.parseInt(myRulesResources.getString("MyCellCol"));
-	}
-	
-	/**
 	 * Creates the type of Cell corresponding to the correct simulation.
 	 * @param initialState: initial state of the Cell.
 	 * @param row: row of the initial location of the Cell.
@@ -86,61 +62,11 @@ public abstract class Rules {
 	}
 	
 	/**
-	 * Returns the default state of the simulation.
-	 * @return default state of the simulation.
+	 * Returns the default state for a new cell for the simulation.
+	 * @return default state for a new cell.
 	 */
 	public String getDefault() {
 		return myDefaultState;
-	}
-	
-	
-	/**
-	 * Counts the number of neighbors of a certain state.
-	 * @param neighborhood: Cell[][] containing neighboring cells to check.
-	 * @param state: state of cells to count.
-	 * @return number of neighbors of a certain state.
-	 */
-	protected int countSurroundingNeighborsOfType(Cell[][] neighborhood, String state) {
-		int ret = 0;
-		for (int row = 0; row < neighborhood.length; row++) {
-			for (int col = 0; col < neighborhood[row].length; col++) {
-				if (neighborhood[row][col] != null) {
-					if (row != myCellRow || col != myCellCol) {
-						if (neighborhood[row][col].getCurState().equals(state)) {
-							ret++;
-						}
-					}
-				}
-			}
-		}
-		return ret;
-	}
-	
-	/**
-	 * Rules for each simulation to be implemented by simulation-specific subclasses.
-	 * @param cell: Cell to apply rules to.
-	 * @param grid: Simulation grid.
-	 */
-	public abstract void applyRulesToCell(Cell cell, Grid grid);
-
-	/**
-	 * Switch cell1 and cell2's states, set their next location's for reference, and add to the list of cells to be updated.
-	 * @param cell1: first cell to be swapped.
-	 * @param cell2: second cell to be swapped.
-	 */
-	protected void switchCells(Cell cell1, Cell cell2) {
-		int cell1Row = cell1.getCurRow();
-		int cell1Col = cell1.getCurCol();
-		String cell1State = cell1.getCurState();
-		
-		cell1.setNextLocation(cell2.getCurRow(), cell2.getCurCol());
-		cell1.setNextState(cell2.getCurState());
-		
-		cell2.setNextLocation(cell1Row, cell1Col);
-		cell2.setNextState(cell1State);
-		
-		addCellToBeUpdated(cell1);
-		addCellToBeUpdated(cell2);
 	}
 	
 	/**
@@ -166,7 +92,9 @@ public abstract class Rules {
 	 * @param cell: Cell that no longer needs to be updated.
 	 */
 	public void removeCellToBeUpdated(Cell cell) {
-		toBeUpdated.remove(cell);
+		if (toBeUpdated.contains(cell)) {
+			toBeUpdated.remove(cell);
+		}
 	}
 	
 	/**
@@ -174,6 +102,26 @@ public abstract class Rules {
 	 */
 	public void clearToBeUpdatedList(){
 		toBeUpdated.clear();
+	}	
+
+	/**
+	 * Populates the initial state and colors information for this simulation into two maps.
+	 */
+	public void populateStatesInfo(){
+		myStatesCount = new HashMap<String, Integer>();
+		myStatesColors = new HashMap<String, Color>();
+		myRulesResources = ResourceBundle.getBundle(DEFAULT_RULES_RESOURCE);
+		String ruleName = toString().replace(" ", "");
+		String[] states = myRulesResources.getString(ruleName + "States").split(",");
+		String[] colors = myRulesResources.getString(ruleName + "Colors").split(",");
+		for(int i = 0; i < states.length; i++){
+			myStatesCount.put(states[i], 0);
+			Color color = Color.web(colors[i]);
+			myStatesColors.put(states[i], color);
+		}
+		myDefaultState = myRulesResources.getString(ruleName + "DefaultState");
+		myCellRow = Integer.parseInt(myRulesResources.getString("MyCellRow"));
+		myCellCol = Integer.parseInt(myRulesResources.getString("MyCellCol"));
 	}
 	
 	/**
@@ -228,6 +176,49 @@ public abstract class Rules {
 		}
 	}
 	
+	
+	/**
+	 * Counts the number of neighbors of a certain state.
+	 * @param neighborhood: Cell[][] containing neighboring cells to check.
+	 * @param state: state of cells to count.
+	 * @return number of neighbors of a certain state.
+	 */
+	protected int countSurroundingNeighborsOfType(Cell[][] neighborhood, String state) {
+		int ret = 0;
+		for (int row = 0; row < neighborhood.length; row++) {
+			for (int col = 0; col < neighborhood[row].length; col++) {
+				if (neighborhood[row][col] != null) {
+					if (row != myCellRow || col != myCellCol) {
+						if (neighborhood[row][col].getCurState().equals(state)) {
+							ret++;
+						}
+					}
+				}
+			}
+		}
+		return ret;
+	}
+	
+	/**
+	 * Switch cell1 and cell2's states, set their next location's for reference, and add to the list of cells to be updated.
+	 * @param cell1: first cell to be swapped.
+	 * @param cell2: second cell to be swapped.
+	 */
+	protected void switchCells(Cell cell1, Cell cell2) {
+		int cell1Row = cell1.getCurRow();
+		int cell1Col = cell1.getCurCol();
+		String cell1State = cell1.getCurState();
+		
+		cell1.setNextLocation(cell2.getCurRow(), cell2.getCurCol());
+		cell1.setNextState(cell2.getCurState());
+		
+		cell2.setNextLocation(cell1Row, cell1Col);
+		cell2.setNextState(cell1State);
+		
+		addCellToBeUpdated(cell1);
+		addCellToBeUpdated(cell2);
+	}
+	
 	/**
 	 * Checks if the cell is the last one in the grid.
 	 * @param cell: cell to check.
@@ -237,7 +228,7 @@ public abstract class Rules {
 	protected boolean isLastCellInGrid(Cell cell, Grid grid) {
 		int offset = 1;
 		if (grid.hasBeenResizedThisStep()) {
-			offset = 2;
+			offset = offset + 1;
 		}
 		return (cell.getCurRow() == (grid.getNumRows() - offset)) && (cell.getCurCol() == (grid.getNumCols() - offset));
 	}
@@ -252,9 +243,14 @@ public abstract class Rules {
 	}
 	
 	/**
-	 * Returns string name of the Simulation
+	 * Generates a random integer between min and max for indexing into a list.
+	 * @param max: size of list you're indexing into.
+	 * @return an integer for the random index.
 	 */
-	public abstract String toString();
+	protected int generateRandomWithinBounds(int min, int max) {
+		return generateRandom(max + 1) + min;
+	}
+	
 	
 	/**
 	 * Retrieves a list of all the rule parameters
@@ -262,4 +258,9 @@ public abstract class Rules {
 	 * An ArrayList of Strings containing the game parameters
 	 */
 	public abstract List<String> getParameters();
+	
+	/**
+	 * Returns string name of the Simulation
+	 */
+	public abstract String toString();
 }
